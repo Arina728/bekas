daafb3932c723754ac96573c75e7a88a7a9f3986
Arina <arias.workplace@gmail.com>
2023-07-07 18:01:28 +0300

    origin

---
+++ js.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+let iconIG = document.querySelector('.iconIG');
+let iconFB = document.querySelector('.iconFB');
+
+iconIG.addEventListener('mouseover', () => {
+    iconIG.src = "icon-instagram2.png";
+});
+iconIG.addEventListener('mouseout', () => {
+    iconIG.src = "icon-instagram1.png";
+});
+
+iconFB.addEventListener('mouseover', () => {
+    iconFB.src = "icon-facebook2.png";
+});
+iconFB.addEventListener('mouseout', () => {
+    iconFB.src = "icon-facebook1.png";
+});
